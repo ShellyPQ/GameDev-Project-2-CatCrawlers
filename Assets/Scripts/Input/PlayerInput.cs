6efc1516800c27d7be116f150dd3ffd731e56ff5
ShellyPQ <michelle.quinones1990@gmail.com>
2025-08-12 00:51:03 +1000

    Basic 2D Player Movement
    - Added a player controller script
    - Imported placeholder cat sprite for player
    - Created movement input system
    - Implemented Jump function
    - 2D ground has been created
    - Gizmo showing if we are grounded or not has been implemented to help debug
    
    TO DO:
    - Look into giving it a 2.5D background (parallax potentially?)

---
+++ Assets/Scripts/Input/PlayerInput.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    #region Variables
+    public static PlayerInput instance;
+
+    [HideInInspector] public PlayerControls playerControls;
+
+    //this will hold the move input vector
+    [HideInInspector] public Vector2 moveInput;
+    #endregion
+
+    #region Awake
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            //ensure there is only one of this
+            Destroy(gameObject);
+        }
+
+        playerControls = new PlayerControls();
+        //Get the vector 2 of in the player input and move it into my moveInput variable
+        playerControls.Movement.Move.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
+    }
+    #endregion
+
+    #region OnEnable and OnDisable
+    private void OnEnable()
+    {
+        playerControls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerControls.Disable();
+    }
+    #endregion
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,164 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    #region Variables
+
+    [Header("Movement Properties")]
+    [Tooltip("Player movements speed")]
+    [SerializeField] private float _playerSpeed = 5f;
+
+    [Header("Jump Properties")]
+    [SerializeField] private float _jumpForce = 5f;
+    [SerializeField] private float _jumpTime = 0.5f;
+
+    [Header("Ground Check Properties")]
+    [SerializeField] private float _extraHeight = 0.25f;
+    [SerializeField] private LayerMask _groundMask;
+
+
+    //Variable that will hold the players sprite renderer
+    private SpriteRenderer _renderer;
+    //Variable that will hold the players rigidbody
+    private Rigidbody2D _rb;
+    //Variable that will hold the players collider
+    private Collider2D _playerCollider;
+    //Variable that will hold the float movement input
+    private float _moveInput;
+
+    //Bool to check if the player is jumping
+    private bool _isJumping;
+    //Bool to check if the player is falling
+    private bool _isFalling;
+    //Bool check to see if the player can jump again
+    private float _jumpTimeCounter;
+
+    private RaycastHit2D _groundHit;
+    #endregion
+
+    #region Awake
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        _renderer = GetComponent<SpriteRenderer>();
+        _playerCollider = GetComponent<Collider2D>();
+    }
+    #endregion
+
+    #region Update
+    private void Update()
+    {
+        Jump();
+    }
+    #endregion
+
+    #region FixedUpdate
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+    #endregion
+
+    #region Movement Functions/Methods
+    private void MovePlayer()
+    {
+        //get a reference to the player input script
+        _moveInput = PlayerInput.instance.moveInput.x;
+
+        _rb.velocity = new Vector2(_moveInput * _playerSpeed, _rb.velocity.y);
+
+        //Have the players sprite face in the direction it is moving (only check if we are moving)
+        if (_moveInput > 0 || _moveInput < 0)
+        {
+            if (_rb.velocity.x > 0.01f)
+            {
+                _renderer.flipX = false;
+            }
+            else if (_rb.velocity.x < -0.01f)
+            {
+                _renderer.flipX = true;
+            }
+        }
+    }
+
+    private void Jump()
+    {
+        //button was just pushed and the player is grounded
+        if (PlayerInput.instance.playerControls.Movement.Jump.WasPressedThisFrame() && GroundCheck())
+        {
+            _isJumping = true;
+            _rb.velocity = new Vector2(_rb.velocity.x, _jumpForce);
+
+            //reset the jump time counter
+            _jumpTimeCounter = _jumpTime;
+        }
+
+        //button is being held
+        if (PlayerInput.instance.playerControls.Movement.Jump.IsPressed())
+        {
+            if (_jumpTimeCounter > 0 && _isJumping)
+            {
+                _rb.velocity = new Vector2(_rb.velocity.x, _jumpForce);
+
+                //decrease the jumptime counter (to ensure we don't float into oblivion)
+                _jumpTimeCounter -= Time.deltaTime;
+            }
+            else
+            {
+                //once the timer runs out the jump ends
+                _isJumping = false;
+            }
+        }
+
+        //button was released this frame
+        if (PlayerInput.instance.playerControls.Movement.Jump.WasReleasedThisFrame())
+        {
+            _isJumping = false;
+        }
+
+        DrawGroundCheck();
+    }
+    #endregion
+
+    #region GroundCheck
+    
+    private bool GroundCheck()
+    {
+        //create a box collider at the players feet to check if the player is grounded
+        _groundHit = Physics2D.BoxCast(_playerCollider.bounds.center, _playerCollider.bounds.size, 0f, Vector2.down, _extraHeight, _groundMask);
+
+        if (_groundHit.collider != null)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    #endregion
+
+    #region Debug Function
+    private void DrawGroundCheck()
+    {
+        Color rayColor;
+
+        if (GroundCheck())
+        {
+            rayColor = Color.green;
+        }
+        else
+        {
+            rayColor = Color.red;
+        }
+
+        Debug.DrawRay(_playerCollider.bounds.center + new Vector3(_playerCollider.bounds.extents.x, 0), Vector2.down * (_playerCollider.bounds.extents.y + _extraHeight), rayColor);
+        Debug.DrawRay(_playerCollider.bounds.center - new Vector3(_playerCollider.bounds.extents.x, 0), Vector2.down * (_playerCollider.bounds.extents.y + _extraHeight), rayColor);
+        Debug.DrawRay(_playerCollider.bounds.center - new Vector3(_playerCollider.bounds.extents.x, _playerCollider.bounds.extents.y + _extraHeight), Vector2.right * (_playerCollider.bounds.extents.x * 2), rayColor);
+
+    }
+    #endregion
+}
